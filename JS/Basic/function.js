08b166fdab908cf213b8509dc20794c40b470ab7
Morning <morning@MStarss-Anar.local>
2022-11-28 12:03:16 +0800

    js

---
+++ JS/Basic/function.js
@@ -0,0 +1,60 @@
+// let name = "Nomin";
+// console.log(name);
+
+// let mynumber1 = 15;
+// let mynumber2 = 20;
+// let mynumber3 = mynumber1 + mynumber2;
+// console.log(mynumber3);
+
+// //variables
+// // var lastname = "Nn";
+
+// //constant
+// const PI = 3.14;
+// //won't work for constant
+// // PI = 3.15;
+// // console.log(PI);
+
+// let num1 = "13",
+//   num2 = "16";
+// let num3 = 45,
+//   num4 = 34;
+// let num5 = '"13"';
+// console.log(num1 + num2);
+// console.log(num3 + num4);
+// console.log(num1 + num4);
+// console.log(num5);
+
+// // // bom
+// // let inputNumber = prompt("enter your age");
+// // // console.log(2022 - inputNumber);
+// // let birthYear = 2022 - inputNumber;
+// // console.log("your birth year is " + birthYear);
+
+// let inputNumber = 22;
+// let birthYear = 2022 - inputNumber;
+// console.log("your birth year is " + birthYear);
+
+// //number string
+// let a = 20;
+// let b = 25;
+// console.log("" + a + b);
+// console.log(a + "" + b);
+// console.log(a + " " + b);
+// console.log(a + b + "");
+
+// //20/7 uldegdel 6-g gargav
+// let num11 = 20;
+// let num12 = 7;
+// console.log(num11 % num12);
+
+// let num15 = 65;
+// console.log(num15- (num15 %) / 10);
+
+// let num100 = prompt("Enter first number");
+// let num101 = prompt("Enter second number");
+let num102 = parseFloat(prompt("Enter first number"));
+let num103 = parseFloat(prompt("Enter second number"));
+
+alert("niilber ni : " + (num102 + num103));
+alert("ehnii toog 2dahi toond huvaahad uldegdel ni:" + (num100 % num101));
