c8cd5b8fb167bb4a722ab45dcceeed42323422bf
nomin.oyu <nomin.oyu@corp.unitel.mn>
2022-11-29 20:26:02 +0800

    cl

--- JS_day_02/con-part-02.js
+++ JS_day_02/con-part-02.js
@@ -6,13 +6,14 @@
 // console.log(parseint(num1 / 3));
 
 //
-let age = 13;
-if (age < 21) {
-  console.log("you are not allowed");
-} else {
-  console.log("your are allowed");
-}
-age < 21;
+// let age = 13;
+// if (age < 21) {
+//   console.log("you are not allowed");
+// } else {
+//   console.log("your are allowed");
+// }
+let age =13;
+
 console.log("you are not allowed");
 console.log("you are allowed");
 condition ? true : false;
