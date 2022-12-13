7870196c1e97e915339ba7a61309198318885463
Morning <morning@MStarss-Anar.local>
2022-12-13 12:48:18 +0800

    color

--- JS_Day09_DOM/Color/function.js
+++ JS_Day09_DOM/Color/function.js
@@ -5,17 +5,15 @@ rootElement.style.width = "450px";
 let r = Math.floor(Math.random(1) * 255);
 let g = Math.floor(Math.random(1) * 255);
 let b = Math.floor(Math.random(1) * 255);
-let z = Math.floor(Math.random(r - 100));
-for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 4; j++) {
-    let newDiv = document.createElement("div");
-    rootElement.appendChild(newDiv);
-    newDiv.style.width = "100px";
-    newDiv.style.height = "100px";
-    newDiv.style.border = "1px solid black";
-    newDiv.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    if (j <= 4 && i <= 4) {
-      newDiv.style.backgroundColor = `rgb(${z}, ${g}, ${b})`;
-    }
+let n = Math.floor(Math.random(0, 1) * 16);
+for (let i = 0; i < 16; i++) {
+  let newDiv = document.createElement("div");
+  rootElement.appendChild(newDiv);
+  newDiv.style.width = "100px";
+  newDiv.style.height = "100px";
+  newDiv.style.border = "1px solid white";
+  newDiv.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  if (n == i) {
+    newDiv.style.backgroundColor = `rgb(${r + 30}, ${g + 30}, ${b + 30})`;
   }
 }
