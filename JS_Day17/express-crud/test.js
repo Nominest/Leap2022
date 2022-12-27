876a9fec3a6ba81452be6f29ae574cc972a0fed5
Nominest <nominest3@gmail.com>
2022-12-28 00:20:01 +0800

    hs

--- JS_Day17/express-crud/test.js
+++ JS_Day17/express-crud/test.js
@@ -1,16 +1,14 @@
 let btn = document.getElementById("allbtn");
 let toyota = document.getElementById("toyota");
+let lexus = document.getElementById("lexus");
 let root = document.getElementById("root");
 let saveBtn = document.getElementById("savebtn");
-let inputModel = document.getElementById("mark");
-// let inputBrand = document.getElementById("brand");
-// let inputImage = document.getElementById("image");
-
+// let inputModel = document.getElementById("mark");
 root.style.display = "flex";
 root.style.flexWrap = "wrap";
 root.style.gap = "10px";
 
-btn.addEventListener("click", () => {
+allbtn.addEventListener("click", () => {
   root.innerHTML = '';
     fetch("http://localhost:3333/api/cars")
     .then((res) => res.json())
@@ -36,27 +34,10 @@ btn.addEventListener("click", () => {
 
 toyota.addEventListener("click", () => {
   root.innerHTML = '';
-  fetch("http://localhost:3333/api/cars")
-    .then((res) => res.json())
-    .then((res) => {fetch("http://localhost:3333/api/cars")
+    fetch("http://localhost:3333/api/cars")
     .then((res) => res.json())
     .then((res) => {
-      
-      res.map((car) => {
-        let newCard = document.createElement("div");
-        newCard.innerHTML = `<div class="card" style="width: 18rem;">
-        <img src="${car.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${car.model}</h5>
-          <p class="card-text">${car.brand}</p>
-          <a href="#" class="btn btn-primary">More</a>
-        </div>
-      </div>`;
-        root.appendChild(newCard);
-      });
-    })
-    .catch((e) => console.log("e: ", e));
-      res.map((car) => {
+            res.map((car) => {
         if(car.brand === "Toyota") {
           let newCard = document.createElement("div");
           newCard.innerHTML = `<div class="card" style="width: 18rem;">
@@ -75,13 +56,88 @@ toyota.addEventListener("click", () => {
     .catch((e) => console.log("e: ", e));
 });
 
-saveBtn.addEventListener("click", () => {
-    fetch('http://localhost:3333/api/cars', {
-    method: 'POST',
-    body: JSON.stringify({ 
-      model: document.getElementById("model").value,
-      brand: document.getElementById("brand").value,
-      image: document.getElementById("image").value })
+lexus.addEventListener("click", () => {
+  root.innerHTML = '';
+    fetch("http://localhost:3333/api/cars")
+    .then((res) => res.json())
+    .then((res) => {
+            res.map((car) => {
+        if(car.brand === "Lexus") {
+          let newCard = document.createElement("div");
+          newCard.innerHTML = `<div class="card" style="width: 18rem;">
+          <img src="${car.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${car.model}</h5>
+            <p class="card-text">${car.brand}</p>
+            <a href="#" class="btn btn-primary">More</a>
+          </div>
+        </div>`;
+        root.appendChild(newCard);
+        } 
+        
+      });
     })
-
+    .catch((e) => console.log("e: ", e));
 });
+
+
+
+
+
+
+
+
+
+
+
+// toyota.addEventListener("click", () => {
+//   root.innerHTML = '';
+//   fetch("http://localhost:3333/api/cars")
+//     .then((res) => res.json())
+//     .then((res) => {fetch("http://localhost:3333/api/cars")
+//     .then((res) => res.json())
+//     .then((res) => {
+      
+//       res.map((car) => {
+//         let newCard = document.createElement("div");
+//         newCard.innerHTML = `<div class="card" style="width: 18rem;">
+//         <img src="${car.image}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">${car.model}</h5>
+//           <p class="card-text">${car.brand}</p>
+//           <a href="#" class="btn btn-primary">More</a>
+//         </div>
+//       </div>`;
+//         root.appendChild(newCard);
+//       });
+//     })
+//     .catch((e) => console.log("e: ", e));
+//       res.map((car) => {
+//         if(car.brand === "Toyota") {
+//           let newCard = document.createElement("div");
+//           newCard.innerHTML = `<div class="card" style="width: 18rem;">
+//           <img src="${car.image}" class="card-img-top" alt="...">
+//           <div class="card-body">
+//             <h5 class="card-title">${car.model}</h5>
+//             <p class="card-text">${car.brand}</p>
+//             <a href="#" class="btn btn-primary">More</a>
+//           </div>
+//         </div>`;
+//         root.appendChild(newCard);
+//         } 
+        
+//       });
+//     })
+//     .catch((e) => console.log("e: ", e));
+// });
+
+// saveBtn.addEventListener("click", () => {
+//     fetch('http://localhost:3333/api/cars', {
+//     method: 'POST',
+//     body: JSON.stringify({ 
+//       model: document.getElementById("model").value,
+//       brand: document.getElementById("brand").value,
+//       image: document.getElementById("image").value })
+//     })
+
+// });
